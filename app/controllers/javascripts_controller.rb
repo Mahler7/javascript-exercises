93b46f63ad4e6f6815320b734dcc1ce6077a6c13
Justin Jungst <jjungstprogramming@gmail.com>
2016-04-21 14:48:43 -0500

    Ch1 Ex1

---
+++ app/controllers/javascripts_controller.rb
@@ -0,0 +1,11 @@
+class JavascriptsController < ApplicationController
+
+  def ch1_ex1_email_list
+
+  end
+
+  def ch1_ex1_email_list_join
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+ 
+# ch1 ex1
+ get '/email_list/ch1/ex1' => 'javascripts#ch1_ex1_email_list'
+ get '/email_list/ch1/join' => 'javascripts#ch1_ex1_email_list_join'
+
+
+end
